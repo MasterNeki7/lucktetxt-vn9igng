3d931e7e36a185e8130c7e51dde0d21c53afd147
Neo Babic <neo.babic@elev.ga.ntig.se>
2022-08-31 14:22:27 +0200

    Walter white breaking bad

---
+++ lucktetxtövn9igng/Program.cs
@@ -0,0 +1,23 @@
+﻿string name;
+
+string virus;
+
+string fluid;
+
+string soup;
+
+Console.WriteLine("Please type your name");
+name = Console.ReadLine();
+
+Console.WriteLine("Please type the name of a virus");
+virus = Console.ReadLine();
+
+Console.WriteLine("Please type the name of a fluid");
+fluid = Console.ReadLine();
+
+Console.WriteLine("Please type the name of a soup");
+soup = Console.ReadLine();
+
+Console.WriteLine($"Hello Mr.Mikael. We are sorry to inform you that {name} is home sick with {virus}. They are currently in the ICU and are being fed {fluid} and {soup}. The doctors excpect them to pass away soon(f)");
+
+Console.ReadLine();
